a94dcd39aaddf01b24b36b7b21c8cf4f70a8a6c9
TheOriginalReben <89179755+TheOriginalReben@users.noreply.github.com>
2023-09-15 06:26:38 +0100

    Add files via upload

--- Task1/Form1.Designer.cs
+++ Task1/Form1.Designer.cs
@@ -33,17 +33,22 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.itemidbox = new System.Windows.Forms.NumericUpDown();
+            this.itemidlabel = new System.Windows.Forms.Label();
+            this.cancelbtn = new System.Windows.Forms.Button();
+            this.confirmbtn = new System.Windows.Forms.Button();
+            this.amountbox = new System.Windows.Forms.NumericUpDown();
+            this.stocklist = new System.Windows.Forms.ListBox();
+            this.idlist = new System.Windows.Forms.ListBox();
+            this.amountlabel = new System.Windows.Forms.Label();
+            this.namelabel = new System.Windows.Forms.Label();
             this.namebox = new System.Windows.Forms.TextBox();
             this.updbtn = new System.Windows.Forms.Button();
             this.rmvbtn = new System.Windows.Forms.Button();
             this.addbtn = new System.Windows.Forms.Button();
             this.namelist = new System.Windows.Forms.ListBox();
-            this.idlist = new System.Windows.Forms.ListBox();
-            this.stocklist = new System.Windows.Forms.ListBox();
-            this.amountbox = new System.Windows.Forms.NumericUpDown();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.itemidbox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.amountbox)).BeginInit();
             this.SuspendLayout();
             // 
@@ -87,11 +92,15 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.itemidbox);
+            this.panel1.Controls.Add(this.itemidlabel);
+            this.panel1.Controls.Add(this.cancelbtn);
+            this.panel1.Controls.Add(this.confirmbtn);
             this.panel1.Controls.Add(this.amountbox);
             this.panel1.Controls.Add(this.stocklist);
             this.panel1.Controls.Add(this.idlist);
-            this.panel1.Controls.Add(this.label5);
-            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.amountlabel);
+            this.panel1.Controls.Add(this.namelabel);
             this.panel1.Controls.Add(this.namebox);
             this.panel1.Controls.Add(this.updbtn);
             this.panel1.Controls.Add(this.rmvbtn);
@@ -105,78 +114,55 @@
             this.panel1.Size = new System.Drawing.Size(413, 313);
             this.panel1.TabIndex = 6;
             // 
-            // label5
+            // itemidbox
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(288, 92);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(43, 13);
-            this.label5.TabIndex = 14;
-            this.label5.Text = "Amount";
+            this.itemidbox.Location = new System.Drawing.Point(288, 133);
+            this.itemidbox.Name = "itemidbox";
+            this.itemidbox.Size = new System.Drawing.Size(122, 20);
+            this.itemidbox.TabIndex = 22;
+            this.itemidbox.Visible = false;
+            this.itemidbox.ValueChanged += new System.EventHandler(this.itemIDupdown_ValueChanged);
             // 
-            // label4
+            // itemidlabel
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(288, 53);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 13;
-            this.label4.Text = "Name";
+            this.itemidlabel.AutoSize = true;
+            this.itemidlabel.Location = new System.Drawing.Point(288, 117);
+            this.itemidlabel.Name = "itemidlabel";
+            this.itemidlabel.Size = new System.Drawing.Size(41, 13);
+            this.itemidlabel.TabIndex = 21;
+            this.itemidlabel.Text = "Item ID";
+            this.itemidlabel.Visible = false;
+            this.itemidlabel.Click += new System.EventHandler(this.itemidlabel_Click);
             // 
-            // namebox
+            // cancelbtn
             // 
-            this.namebox.Location = new System.Drawing.Point(288, 69);
-            this.namebox.Name = "namebox";
-            this.namebox.Size = new System.Drawing.Size(122, 20);
-            this.namebox.TabIndex = 10;
-            this.namebox.TextChanged += new System.EventHandler(this.namebox_TextChanged);
+            this.cancelbtn.Location = new System.Drawing.Point(350, 277);
+            this.cancelbtn.Name = "cancelbtn";
+            this.cancelbtn.Size = new System.Drawing.Size(50, 23);
+            this.cancelbtn.TabIndex = 20;
+            this.cancelbtn.Text = "Cancel";
+            this.cancelbtn.UseVisualStyleBackColor = true;
+            this.cancelbtn.Visible = false;
+            this.cancelbtn.Click += new System.EventHandler(this.cancelbtn_Click);
             // 
-            // updbtn
+            // confirmbtn
             // 
-            this.updbtn.Location = new System.Drawing.Point(313, 205);
-            this.updbtn.Name = "updbtn";
-            this.updbtn.Size = new System.Drawing.Size(75, 23);
-            this.updbtn.TabIndex = 9;
-            this.updbtn.Text = "Update Item";
-            this.updbtn.UseVisualStyleBackColor = true;
-            this.updbtn.Click += new System.EventHandler(this.updbtn_Click);
+            this.confirmbtn.Location = new System.Drawing.Point(291, 277);
+            this.confirmbtn.Name = "confirmbtn";
+            this.confirmbtn.Size = new System.Drawing.Size(53, 23);
+            this.confirmbtn.TabIndex = 19;
+            this.confirmbtn.Text = "Confirm";
+            this.confirmbtn.UseVisualStyleBackColor = true;
+            this.confirmbtn.Visible = false;
+            this.confirmbtn.Click += new System.EventHandler(this.confirmbtn_Click);
             // 
-            // rmvbtn
+            // amountbox
             // 
-            this.rmvbtn.Location = new System.Drawing.Point(313, 176);
-            this.rmvbtn.Name = "rmvbtn";
-            this.rmvbtn.Size = new System.Drawing.Size(75, 23);
-            this.rmvbtn.TabIndex = 8;
-            this.rmvbtn.Text = "Remove Item";
-            this.rmvbtn.UseVisualStyleBackColor = true;
-            this.rmvbtn.Click += new System.EventHandler(this.rmvbtn_Click);
-            // 
-            // addbtn
-            // 
-            this.addbtn.Location = new System.Drawing.Point(313, 147);
-            this.addbtn.Name = "addbtn";
-            this.addbtn.Size = new System.Drawing.Size(75, 23);
-            this.addbtn.TabIndex = 7;
-            this.addbtn.Text = "Add Item";
-            this.addbtn.UseVisualStyleBackColor = true;
-            this.addbtn.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // namelist
-            // 
-            this.namelist.FormattingEnabled = true;
-            this.namelist.Location = new System.Drawing.Point(53, 20);
-            this.namelist.Name = "namelist";
-            this.namelist.Size = new System.Drawing.Size(174, 290);
-            this.namelist.TabIndex = 0;
-            this.namelist.SelectedIndexChanged += new System.EventHandler(this.stocklist_SelectedIndexChanged);
-            // 
-            // idlist
-            // 
-            this.idlist.FormattingEnabled = true;
-            this.idlist.Location = new System.Drawing.Point(6, 20);
-            this.idlist.Name = "idlist";
-            this.idlist.Size = new System.Drawing.Size(41, 290);
-            this.idlist.TabIndex = 15;
+            this.amountbox.Location = new System.Drawing.Point(288, 90);
+            this.amountbox.Name = "amountbox";
+            this.amountbox.Size = new System.Drawing.Size(122, 20);
+            this.amountbox.TabIndex = 17;
+            this.amountbox.Visible = false;
             // 
             // stocklist
             // 
@@ -186,12 +172,84 @@
             this.stocklist.Size = new System.Drawing.Size(49, 290);
             this.stocklist.TabIndex = 16;
             // 
-            // amountbox
+            // idlist
             // 
-            this.amountbox.Location = new System.Drawing.Point(288, 108);
-            this.amountbox.Name = "amountbox";
-            this.amountbox.Size = new System.Drawing.Size(122, 20);
-            this.amountbox.TabIndex = 17;
+            this.idlist.FormattingEnabled = true;
+            this.idlist.Location = new System.Drawing.Point(6, 20);
+            this.idlist.Name = "idlist";
+            this.idlist.Size = new System.Drawing.Size(41, 290);
+            this.idlist.TabIndex = 15;
+            this.idlist.SelectedIndexChanged += new System.EventHandler(this.idlist_SelectedIndexChanged);
+            // 
+            // amountlabel
+            // 
+            this.amountlabel.AutoSize = true;
+            this.amountlabel.Location = new System.Drawing.Point(288, 74);
+            this.amountlabel.Name = "amountlabel";
+            this.amountlabel.Size = new System.Drawing.Size(43, 13);
+            this.amountlabel.TabIndex = 14;
+            this.amountlabel.Text = "Amount";
+            this.amountlabel.Visible = false;
+            this.amountlabel.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // namelabel
+            // 
+            this.namelabel.AutoSize = true;
+            this.namelabel.Location = new System.Drawing.Point(288, 35);
+            this.namelabel.Name = "namelabel";
+            this.namelabel.Size = new System.Drawing.Size(35, 13);
+            this.namelabel.TabIndex = 13;
+            this.namelabel.Text = "Name";
+            this.namelabel.Visible = false;
+            this.namelabel.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // namebox
+            // 
+            this.namebox.Location = new System.Drawing.Point(288, 51);
+            this.namebox.Name = "namebox";
+            this.namebox.Size = new System.Drawing.Size(122, 20);
+            this.namebox.TabIndex = 10;
+            this.namebox.Visible = false;
+            this.namebox.TextChanged += new System.EventHandler(this.namebox_TextChanged);
+            // 
+            // updbtn
+            // 
+            this.updbtn.Location = new System.Drawing.Point(313, 230);
+            this.updbtn.Name = "updbtn";
+            this.updbtn.Size = new System.Drawing.Size(75, 23);
+            this.updbtn.TabIndex = 9;
+            this.updbtn.Text = "Update Item";
+            this.updbtn.UseVisualStyleBackColor = true;
+            this.updbtn.Click += new System.EventHandler(this.updbtn_Click);
+            // 
+            // rmvbtn
+            // 
+            this.rmvbtn.Location = new System.Drawing.Point(313, 201);
+            this.rmvbtn.Name = "rmvbtn";
+            this.rmvbtn.Size = new System.Drawing.Size(75, 23);
+            this.rmvbtn.TabIndex = 8;
+            this.rmvbtn.Text = "Remove Item";
+            this.rmvbtn.UseVisualStyleBackColor = true;
+            this.rmvbtn.Click += new System.EventHandler(this.rmvbtn_Click);
+            // 
+            // addbtn
+            // 
+            this.addbtn.Location = new System.Drawing.Point(313, 172);
+            this.addbtn.Name = "addbtn";
+            this.addbtn.Size = new System.Drawing.Size(75, 23);
+            this.addbtn.TabIndex = 7;
+            this.addbtn.Text = "Add Item";
+            this.addbtn.UseVisualStyleBackColor = true;
+            this.addbtn.Click += new System.EventHandler(this.addbtn_Click);
+            // 
+            // namelist
+            // 
+            this.namelist.FormattingEnabled = true;
+            this.namelist.Location = new System.Drawing.Point(53, 20);
+            this.namelist.Name = "namelist";
+            this.namelist.Size = new System.Drawing.Size(174, 290);
+            this.namelist.TabIndex = 0;
+            this.namelist.SelectedIndexChanged += new System.EventHandler(this.stocklist_SelectedIndexChanged);
             // 
             // Form1
             // 
@@ -204,6 +262,7 @@
             this.Text = "Inventory Management System";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.itemidbox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.amountbox)).EndInit();
             this.ResumeLayout(false);
 
@@ -217,8 +276,8 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.ListBox namelist;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label amountlabel;
+        private System.Windows.Forms.Label namelabel;
         private System.Windows.Forms.TextBox namebox;
         private System.Windows.Forms.Button updbtn;
         private System.Windows.Forms.Button rmvbtn;
@@ -226,6 +285,10 @@
         private System.Windows.Forms.ListBox stocklist;
         private System.Windows.Forms.ListBox idlist;
         private System.Windows.Forms.NumericUpDown amountbox;
+        private System.Windows.Forms.Button cancelbtn;
+        private System.Windows.Forms.Button confirmbtn;
+        private System.Windows.Forms.NumericUpDown itemidbox;
+        private System.Windows.Forms.Label itemidlabel;
     }
 }
 
--- Task1/Form1.cs
+++ Task1/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -14,6 +15,7 @@ namespace Task1
     {
         private Tools tools = new Tools();
         int id = 0;
+        int selection = 0;
 
         public Form1()
         {
@@ -35,26 +37,20 @@ namespace Task1
 
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void addbtn_Click(object sender, EventArgs e)
         {
-            if (string.IsNullOrWhiteSpace(namebox.Text))
-                MessageBox.Show("Error! The name should not be empty!!!");
-            else
-            if (namelist.Items.Contains(namebox.Text))
-                MessageBox.Show("Error! You cannot add that item. Item already exists!");
-            else
-            if (string.IsNullOrWhiteSpace(amountbox.Text))
+            this.addbtn.Visible = false;
+            this.rmvbtn.Visible = false;
+            this.updbtn.Visible = false;
+            this.confirmbtn.Visible = true;
+            this.cancelbtn.Visible = true;
+            this.namelabel.Visible = true;
+            this.namebox.Visible = true;
+            this.amountlabel.Visible = true;
+            this.amountbox.Visible = true;
+            if (namebox.Modified == true)
             {
-                MessageBox.Show("Error! The item amount should exceed 0.");
-            }
-            else
-            {
-                idlist.Items.Add(id);
-                namelist.Items.Add(namebox.Text);
-                stocklist.Items.Add(amountbox.Text);
-                namebox.Clear();
-                amountbox.ResetText();
-                id++;
+                selection = 0;
             }
         }
 
@@ -70,12 +66,38 @@ namespace Task1
 
         private void rmvbtn_Click(object sender, EventArgs e)
         {
+            this.addbtn.Visible = false;
+            this.rmvbtn.Visible = false;
+            this.updbtn.Visible = false;
+            this.itemidlabel.Visible = true;
+            this.itemidbox.Visible = true;
+            this.confirmbtn.Visible = true;
+            this.cancelbtn.Visible = true;
+            this.namelabel.Visible = false;
+            this.namebox.Visible = false;
+            this.amountlabel.Visible = false;
+            this.amountbox.Visible = false;
+            selection = 1;
 
         }
 
         private void updbtn_Click(object sender, EventArgs e)
         {
-
+            this.addbtn.Visible = false;
+            this.rmvbtn.Visible = false;
+            this.updbtn.Visible = false;
+            this.confirmbtn.Visible = true;
+            this.cancelbtn.Visible = true;
+            this.itemidlabel.Visible = true;
+            this.itemidbox.Visible = true;
+            this.namelabel.Visible = true;
+            this.namebox.Visible = true;
+            this.amountlabel.Visible = true;
+            this.amountbox.Visible = true;
+            if (namebox.Modified == true)
+            {
+                selection = 2;
+            }
         }
 
         private void tableLayoutPanel1_Paint_1(object sender, PaintEventArgs e)
@@ -89,6 +111,185 @@ namespace Task1
         }
 
         private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cancelbtn_Click(object sender, EventArgs e)
+        {
+            namebox.Clear();
+            amountbox.ResetText();
+            this.addbtn.Visible = true;
+            this.rmvbtn.Visible = true;
+            this.updbtn.Visible = true;
+            this.itemidlabel.Visible = false;
+            this.itemidbox.Visible = false;
+            this.confirmbtn.Visible = false;
+            this.cancelbtn.Visible = false;
+            this.namelabel.Visible = false;
+            this.namebox.Visible = false;
+            this.amountlabel.Visible = false;
+            this.amountbox.Visible = false;
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void itemidlabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void itemIDupdown_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void confirmbtn_Click(object sender, EventArgs e)
+        {
+            if (selection == 0)
+            {
+                if (string.IsNullOrWhiteSpace(namebox.Text))
+                    MessageBox.Show("Error! The name should not be empty!!!");
+                else
+                if (namelist.Items.Contains(namebox.Text))
+                    MessageBox.Show("Error! You cannot add that item. Item already exists!");
+                else
+                if (string.IsNullOrWhiteSpace(amountbox.Text))
+                {
+                    MessageBox.Show("Error! The item amount should not be empty!");
+                }
+                else
+                if (int.Parse(amountbox.Text).Equals(0))
+                {
+                    MessageBox.Show("Error! The item amount should exceed 0");
+                }
+                else
+                {
+                    idlist.Items.Add(id);
+                    namelist.Items.Add(namebox.Text);
+                    stocklist.Items.Add(amountbox.Text);
+                    namebox.Clear();
+                    amountbox.ResetText();
+                    id++;
+                }
+            }
+            else
+            if (selection == 1)
+            {
+                if (string.IsNullOrWhiteSpace(itemidbox.Text))
+                    MessageBox.Show("Error! The ID box should not be empty!!!");
+                else
+                if (!idlist.Items.Contains(int.Parse(itemidbox.Text)))
+                    MessageBox.Show("Error! You cannot remove something that does not exists!");
+                else
+                {
+                    idlist.Items.RemoveAt(int.Parse(itemidbox.Text));
+                    namelist.Items.RemoveAt(int.Parse(itemidbox.Text));
+                    stocklist.Items.RemoveAt(int.Parse(itemidbox.Text));
+                    idlist.ResetText();
+                    namelist.Update();
+                    stocklist.Update();
+                    namebox.Clear();
+                    amountbox.ResetText();
+                    id++;
+                }
+            }
+            else if (selection == 2)
+            {
+                if (string.IsNullOrWhiteSpace(namebox.Text))
+                {
+                    MessageBox.Show("Error! The name should not be empty!!!");
+                }
+                else if (namelist.Items.Contains(namebox.Text))
+                {
+                    MessageBox.Show("Error! You cannot add that item. Item already exists!");
+                }
+                else if (string.IsNullOrWhiteSpace(amountbox.Text))
+                {
+                    MessageBox.Show("Error! The item amount should not be empty!");
+                }
+                else if (int.Parse(amountbox.Text) <= 0)
+                {
+                    MessageBox.Show("Error! The item amount should exceed 0");
+                }
+                else
+                {
+                    int index = namelist.Items.IndexOf(namebox.Text);
+                    if (index != -1)
+                    {
+                        stocklist.Items[index] = amountbox.Text;
+                    }
+                    else
+                    {
+                        idlist.Items.Add(itemidbox);
+                        namelist.Items.Add(namebox.Text);
+                        stocklist.Items.Add(amountbox.Text);
+                        id++;
+                    }
+
+                    namebox.Clear();
+                    amountbox.ResetText();
+                }
+                /*
+                if (string.IsNullOrWhiteSpace(namebox.Text))
+                    MessageBox.Show("Error! The name should not be empty!!!");
+                else
+                if (namelist.Items.Contains(namebox.Text))
+                    MessageBox.Show("Error! You cannot add that item. Item already exists!");
+                else
+                if (string.IsNullOrWhiteSpace(amountbox.Text))
+                {
+                    MessageBox.Show("Error! The item amount should not be empty!");
+                }
+                else
+                if (int.Parse(amountbox.Text).Equals(0))
+                {
+                    MessageBox.Show("Error! The item amount should exceed 0");
+                }
+                else
+                {
+                    idlist.Items.Add(itemidbox);
+                    namelist.Items.Add(namebox.Text);
+                    stocklist.Items.Add(amountbox.Text);
+                    namebox.Clear();
+                    amountbox.ResetText();
+                    id++;
+                }*/
+            }
+            else
+            {
+                /*this.addbtn.Visible = true;
+                this.rmvbtn.Visible = true;
+                this.updbtn.Visible = true;
+                this.confirmbtn.Visible = false;
+                this.cancelbtn.Visible = false;
+                this.namelabel.Visible = false;
+                this.namebox.Visible = false;
+                this.amountlabel.Visible = false;
+                this.amountbox.Visible = false;*/
+            }
+            this.addbtn.Visible = true;
+            this.rmvbtn.Visible = true;
+            this.updbtn.Visible = true;
+            this.itemidlabel.Visible = false;
+            this.itemidbox.Visible = false;
+            this.confirmbtn.Visible = false;
+            this.cancelbtn.Visible = false;
+            this.namelabel.Visible = false;
+            this.namebox.Visible = false;
+            this.amountlabel.Visible = false;
+            this.amountbox.Visible = false;
+        }
+
+        private void idlist_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
